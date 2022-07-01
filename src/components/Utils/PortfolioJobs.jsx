c439b80f67a87797124b0704c2600dbba438de9f
gabg4224 <gabg4224@gmail.com>
2022-07-01 08:10:16 -0300

    new update

--- src/components/Utils/PortfolioJobs.jsx
+++ src/components/Utils/PortfolioJobs.jsx
@@ -1,5 +1,6 @@
 import tiktakSrc from "../../assets/tiktak.png";
 import sketch from "../../assets/sketch.png";
+import clickCounter from "../../assets/click-counter.png";
 const PortfolioJobs = [
   {
     id: 1,
@@ -14,7 +15,14 @@ const PortfolioJobs = [
     title: "sketch",
     repository: "https://github.com/gabg4224/Sketch",
     liveDemo: "https://gabg4224.github.io/Sketch/",
-  }
+  },
+  {
+    id: 3,
+    img: clickCounter,
+    title: "click-counter",
+    repository: "https://github.com/gabg4224/click-counter/tree/master",
+    liveDemo: "https://gabg4224.github.io/click-counter/",
+  },
   
 ];
 
